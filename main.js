f06def899d9eb500ba890b16d623e8625f17a3e7
dstewart922 <dewaynestewart@hotmail.com>
2016-01-19 14:23:03 -0500

    final commit

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-var images = ['../Girlenes/images/pic1.jpg', '../Girlenes/images/pic2.jpg', '../Girlenes/images/pic3.jpg', '../Girlenes/images/pic4.jpg']
+$( document ).ready(function() {
 
-var index = 0
-$('#next-button').click(function() {
-  index=index+1
-$('img').attr('src', images[index])
+
+	var images = ['../Girlenes/images/pic1.jpg', '../Girlenes/images/pic2.jpg', '../Girlenes/images/pic3.jpg', '../Girlenes/images/pic4.jpg', '../Girlenes/images/pic5.jpg']
+	var index = Math.floor(Math.random()*images.length)
+	
+	
+	$('.photo').attr('src', images[index]);
+
+	function nextCarouselImage() {
+		console.log("next clicked");
+		//index=index+1;
+
+		index=index+1;
+		if(index > images.length - 1){
+		index = 0
+		}
+		console.log(index);
+		$('.photo').attr('src', images[index]);
+		console.log($('.photo').attr('src'))
+	}
+
+	function prevCarouselImage() {
+		index=index-1;
+		if(index < 0){
+		index = images.length - 1
+		}
+		$('.photo').attr('src', images[index])
+	}
+
+
+	$('#next-button').click(nextCarouselImage)
+
+	$('#previous-button').click(prevCarouselImage)
+	setInterval(nextCarouselImage, 5000)
+
+	$(".hamburger").click(function(e) {
+		$(".menu" ).slideToggle("slow");
+	});
 })
-
-$('#previous-button').click(function() {
-  index=index-1
-$('img').attr('src', images[index])
-})
-
-$(".hamburger").click(function(e) {
-	$(".menu" ).slideToggle("slow");
-});
